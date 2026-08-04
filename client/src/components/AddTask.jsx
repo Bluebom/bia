c76d1dcf408d078ad43baab8e93c36ded8cdf505
Franklin Henrique <franklinhpf@hotmail.com>
2026-08-04 21:32:07 +0000

    Add V2 to btn and update the second service of infra with alb

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar task com CI/CD
+        Adicionar task com CI/CD V2
       </button>
       
       <Modal
